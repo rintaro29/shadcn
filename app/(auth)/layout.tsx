209bb03b7d7cb82f0aa5d4784135df7eec5b1144
rintaro1029 <tomitaka1029@gmail.com>
2023-10-25 17:27:27 +0900

    認証機能の追加

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-red-600 h-screen">{children}</div>;
+  return <div className="h-screen flex justify-center items-center">{children}</div>;
 };
 
 export default AuthLayout;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,6 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "../../../components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>This is a protected page</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
