838bb014949e763d536f306ddedde4804f1534fa
rintaro1029 <tomitaka1029@gmail.com>
2023-10-27 20:41:48 +0900

    .env fix

--- lib/db.ts
+++ lib/db.ts
@@ -4,9 +4,9 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = global.prisma || new PrismaClient();
+export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prisma = db;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
 //	開発時は最初に一度だけ初期化して以降は`global`から再利用
 //それ以外は常に初期化
